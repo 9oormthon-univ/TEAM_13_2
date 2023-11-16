a2493e992a75b6396f9cd71da53d20a1da489fb4
djdongjae <jk47wee@gmail.com>
2023-11-17 03:27:55 +0900

    fix: cors 설정

--- src/main/java/com/groom/cookiehouse/CookieHouseApplication.java
+++ src/main/java/com/groom/cookiehouse/CookieHouseApplication.java
@@ -20,9 +20,9 @@ public class CookieHouseApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5173/");
             }
         };
     }
-
 }
